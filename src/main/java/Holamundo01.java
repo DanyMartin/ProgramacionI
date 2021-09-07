860225f803f24045f421f6ce71be76fdf69042d8
DanyMartin <dmartino@ucundinamarca.edu.co>
2021-09-07 11:42:27 -0500

    ''

---
+++ src/main/java/Holamundo01.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+
+
+
+public class Holamundo01 {
+
+    public static void main(String[] args) {
+      Scanner entrada = new Scanner(System.in);
+      int numero;
+        System.out.print("Escriba un numero: ");
+        numero= entrada.nextInt();
+        System.out.println("El numero es; "+numero);
+      
+    }
+}
+
